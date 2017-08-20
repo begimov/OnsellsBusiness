ab8330b8eaac720bcafb1dd87b4d44e0fc814997
begimov <begimov@gmail.com>
2017-08-20 13:05:41 +0300

    basic meta tags added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('titleandmetatags')
+  @include('layouts.partials._metatags')
+@endsection
+
 @section('head-scripts')
 <!-- Google ReCaptcha -->
 <script src='https://www.google.com/recaptcha/api.js'></script>
--- resources/views/welcome/layouts/layout.blade.php
+++ resources/views/welcome/layouts/layout.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Onsells</title>
+  @include('layouts.partials._metatags')
   <!-- Styles -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link href="{{ asset('css/welcome/base.css') }}" rel="stylesheet">
