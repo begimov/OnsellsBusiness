dce00ef6bb8acbf0987f4557e2e2ed5f527f2f9d
begimov <begimov@gmail.com>
2017-08-27 12:33:17 +0300

    check for insufficient balance to checkout added

--- app/Http/Controllers/Webapi/CheckoutController.php
+++ app/Http/Controllers/Webapi/CheckoutController.php
@@ -4,21 +4,36 @@ namespace App\Http\Controllers\Webapi;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Promotions\Application;
 
 class CheckoutController extends Controller
 {
     public function checkout(Request $request)
     {
-        // validation of appplications array
+        //TODO: validation of appplications array
 
         $user = $request->user();
+        $applicationIds = $request->all()['applications'];
 
-        // check if all recieved app ids belongs to $user's owned promotions
+        $applications = Application::with(['user'])
+            ->whereIn('id', $applicationIds)
+            ->get();
+
+        //TODO: check if all recieved app ids belongs to $user's owned promotions
+        // POLICY???
 
         // check if balance amount is enough to pay
+        if (isBalanceSufficient($applications->count(), $user->balance->amount)) {
+
+        }
 
         // touch all recieved apps with paid = 1
 
         // response with status => 'OK'
+    }
+
+    public function isBalanceSufficient($appsCount, $balanceAmount)
+    {
+        return $appsCount * config('promotions.app_base_price') <= $balanceAmount;
     }
 }
