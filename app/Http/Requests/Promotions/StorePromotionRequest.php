70870dfc09a5572933b44abe1835411607e8f993
begimov <begimov@gmail.com>
2017-09-03 08:23:17 +0300

    image upload size limit raised up to 10Mb

--- app/Http/Requests/Promotions/StorePromotionRequest.php
+++ app/Http/Requests/Promotions/StorePromotionRequest.php
@@ -24,7 +24,7 @@ class StorePromotionRequest extends FormRequest
     public function rules()
     {
         return [
-            'image' => 'required|image:max:2048',
+            'image' => 'required|image:max:10240',
             'category' => 'required|integer',
             'company' => 'required|max:255',
             'promotionname' => 'required|max:255',
