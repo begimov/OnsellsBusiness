2a9e7fb9cd9ff2604e72a7c28185a40c21a6118d
begimov <begimov@gmail.com>
2017-09-03 08:32:36 +0300

    form request to validate incoming app ids list added

---
+++ app/Http/Requests/Webapi/CheckoutRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Webapi;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CheckoutRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        //TODO: check if all recieved app ids belongs to $user's owned promotions
+        // POLICY???
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
