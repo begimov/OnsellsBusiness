96c4573fbcae85dee9dff141f7b2771a85287c3c
begimov <begimov@gmail.com>
2017-09-16 09:16:01 +0300

    di and handle method implemetation added in ProcessUploadedPromotionImage job class

--- app/Jobs/Promotions/ProcessUploadedPromotionImage.php
+++ app/Jobs/Promotions/ProcessUploadedPromotionImage.php
@@ -7,19 +7,25 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use App\Classes\Images\ImageProcessor;
 
 class ProcessUploadedPromotionImage implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $pathToOriginal;
+
+    protected $promotion;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($pathToOriginal, $promotion)
     {
-        //
+        $this->pathToOriginal = $pathToOriginal;
+        $this->promotion = $promotion;
     }
 
     /**
@@ -27,8 +33,8 @@ class ProcessUploadedPromotionImage implements ShouldQueue
      *
      * @return void
      */
-    public function handle()
+    public function handle(ImageProcessor $imageProcessor)
     {
-        //
+        $imageProcessor->resizeAndSaveImages($this->pathToOriginal, $this->promotion);
     }
 }
